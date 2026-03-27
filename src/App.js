ef66b4a1ba167ea82dab04e0e34d5696cdad7106
PraharshPatni13 <praharshpatni@gmail.com>
2026-03-27 14:40:22 +0530

    routes commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Contact from "./components/Contact/Contact";
-import Portfolio from "./components/Portfolio/Portfolio";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
-      {/* <Route path="/portfolio" element={<Portfolio />} /> */}
     </Routes>
   );
 }
